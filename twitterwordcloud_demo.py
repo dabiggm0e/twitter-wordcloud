f5ac1eb21643ff90aca5edeb9923a76babe06856
Mo <dabiggmoe@gmail.com>
2021-07-19 04:28:09 +0300

    addeed validation of the passed twitter user

--- twitterwordcloud_demo.py
+++ twitterwordcloud_demo.py
@@ -8,13 +8,14 @@ OUTPUT_FOLDER = "./png"
 SCREENNAME = "twitter"
 
 def parse_args():
+	global SCREENNAME
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-u", "--user", help = "The Twitter user's handle")
 
 	args = parser.parse_args()
-
 	if args.user:
 		SCREENNAME = args.user
+
 		print("Entered username is: " + args.user)
 
 def tokenize_tweets(tweets):
@@ -31,8 +32,15 @@ def main():
 
 	wc = wordcloud_wrapper.WordCloudWrapper(INPUT_FOLDER, OUTPUT_FOLDER, blacklist)
 	tweepy = tweepy_wrapper.TweepyWrapper(TWITTER_SECRETS_PATH)
+	user_model = None
 
+	# Validate that the twitter user exists
+	try:
+		user_model = tweepy.get_api().get_user(screen_name = SCREENNAME)
+	except:
+		exit("FATAL: User " + SCREENNAME + " does not exist.")
 	
+
 	tweets = tweepy.get_user_timeline(id=SCREENNAME, tweet_mode="extended", include_rts=False, limit=10)
 	tokens = tokenize_tweets(tweets)
 
